f4de6c885d16321f32f16486939072c5f59fc5f7
Christian Javellana <cjjavellana@gmail.com>
2017-03-23 23:34:45 +0800

    Perform unit test without requiring database connection, added getDemo test

--- src/test/java/org/cjavellana/controllers/api/v1/DocumentControllerTest.java
+++ src/test/java/org/cjavellana/controllers/api/v1/DocumentControllerTest.java
@@ -1,49 +1,65 @@
 package org.cjavellana.controllers.api.v1;
 
-import org.cjavellana.App;
+import org.cjavellana.services.UserInfoService;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
-import org.springframework.web.context.WebApplicationContext;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 
+import static java.util.Arrays.asList;
 import static org.hamcrest.CoreMatchers.is;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = App.class)
-@WebAppConfiguration
+@WebMvcTest(DocumentController.class)
 public class DocumentControllerTest {
 
+    @Autowired
     private MockMvc mockMvc;
 
-    @Autowired
-    private WebApplicationContext webApplicationContext;
+    @MockBean
+    private UserInfoService userInfoService;
 
     @Before
-    public void setup() throws Exception {
-        this.mockMvc = webAppContextSetup(webApplicationContext).build();
+    public void setup() {
+        given(userInfoService.findAll())
+                .willReturn(new ArrayList<>());
+    }
+
+    @Test
+    public void getDemoTest() throws Exception {
+        mockMvc.perform(get("/api/v1/documents"))
+                .andExpect(jsonPath("$.status", is("ok")))
+                .andExpect(status().isOk());
     }
 
     @Test
     public void putDemo() throws Exception {
         MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
-        map.put("content", Arrays.asList("test"));
+        map.put("content", asList("test"));
 
-        mockMvc.perform(put("/api/v1/document/{id}", 1)
-                .params(map)
-        ).andExpect(jsonPath("$.status", is("test")));
+        MockHttpServletRequestBuilder requestBuilder = put("/api/v1/document/{id}", 1)
+                .params(map);
+        mockMvc.perform(requestBuilder)
+                .andExpect(jsonPath("$.status", is("test")))
+                .andExpect(status().isOk());
+
     }
 
 }
